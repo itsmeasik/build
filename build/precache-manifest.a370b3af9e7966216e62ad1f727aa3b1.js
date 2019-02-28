5d9dcdf591e68868561e1eb712873c30f7a8c92e
Asik Mydeen <ammydeen@amazon.com>
2019-02-28 20:15:22 +0530

    Build success

---
+++ build/precache-manifest.a370b3af9e7966216e62ad1f727aa3b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec29321578076641e841",
+    "url": "/static/css/main.f18aab87.chunk.css"
+  },
+  {
+    "revision": "ec29321578076641e841",
+    "url": "/static/js/main.ec293215.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "18dc2d8d825a7861e3c356ddb100920c",
+    "url": "/index.html"
+  }
+];
